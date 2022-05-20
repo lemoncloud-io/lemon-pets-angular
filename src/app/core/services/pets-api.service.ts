464237b4832f1a89e67fb9cbc115c1fd7f020ee5
unknown <parikshit@lemoncloud.io>
2022-05-20 18:06:42 +0530

    Added functionality of select topic and make basic structure for upload images

--- src/app/core/services/pets-api.service.ts
+++ src/app/core/services/pets-api.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Params } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import {LemonAuthService} from "./lemon-auth.service";
-import {UtilsService} from "./utils.service";
-import {environment} from "@env/environment";
-import {delay} from "rxjs";
+import { LemonAuthService } from './lemon-auth.service';
+import { UtilsService } from './utils.service';
+import { environment } from '@env/environment';
+import { delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -34,4 +34,17 @@ export class PetsApiService {
     );
   }
 
+  fetchproduct$(params: Params) {
+    // if (this.isLocal) {
+    //   const url = 'assets/json/contents.json';
+    //   return this.mockClient.get(url).pipe(delay(200));
+    // }
+
+    return this.httpClient.request$(
+      'GET',
+      environment.petsApiEndpoint,
+      '/contents/1000387',
+      this.utils.deleteUndefinedProperty(params)
+    );
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Content, Items} from './items.model';
-import {PetsApiService} from "@core/services/pets-api.service";
-import {takeUntil} from "rxjs/operators";
-import {ReplaySubject} from "rxjs";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Content } from './items.model';
+import { PetsApiService } from '@core/services/pets-api.service';
+import { takeUntil } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -11,10 +11,7 @@ import {ReplaySubject} from "rxjs";
 })
 export class MainComponent implements OnInit, OnDestroy {
   DEFAULT_IMAGE = './assets/dog_profile.png';
-  items: Items[] = [
-    new Items('Product1', '125', '12.12.2022'),
-    new Items('Product2', '122', '14.12.2022'),
-  ];
+
   fetchedContents: Content[] = [];
 
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
@@ -23,12 +20,13 @@ export class MainComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     const defaultParams = { page: 0, limit: 10 };
-    this.petsApiService.fetchContents$(defaultParams)
+    this.petsApiService
+      .fetchContents$(defaultParams)
       .pipe(takeUntil(this.destroyed$))
       .subscribe(({ list }) => {
         console.log(list);
         this.fetchedContents = list;
-      })
+      });
   }
 
   ngOnDestroy() {
