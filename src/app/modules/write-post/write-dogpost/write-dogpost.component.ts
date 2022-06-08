34320ddb01638071aa0677266f06c2f6809ae2c8
unknown <parikshit@lemoncloud.io>
2022-06-08 18:10:25 +0530

    work on profile image and product post API integration

--- src/app/modules/write-post/write-dogpost/write-dogpost.component.ts
+++ src/app/modules/write-post/write-dogpost/write-dogpost.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PublicApisService } from '../../../services/public-apis.service';
 import { HttpClient } from '@angular/common/http';
+import { ImageApiService } from '@app/core/services/image-api.service';
+import { LemonAuthService } from '@app/core/services/lemon-auth.service';
+import { toArray } from 'rxjs';
 
 @Component({
   selector: 'app-write-dogpost',
@@ -12,10 +15,16 @@ export class WriteDogpostComponent {
   selectedTopic = '';
   selectedValue = '';
   imagestoShow = [];
-  message: string = '';
-  strings = ['a', 'b', 'c'];
+  message: string = 'testing';
+  tags = ['a', 'b', 'c'];
+  subject: string = 'Dog';
 
-  constructor(private action: PublicApisService, private http: HttpClient) {
+  constructor(
+    private action: PublicApisService,
+    private http: HttpClient,
+    private imgupload: ImageApiService,
+    private authRequest: LemonAuthService
+  ) {
     this.action.getlistCategory().subscribe((data) => {
       this.listCategories = data;
       console.log(this.listCategories);
@@ -53,35 +62,18 @@ export class WriteDogpostComponent {
 
   getUserFormData(data: any) {}
 
-  async onproductUpload() {
-    // this.action.uploadedProfileimage.next(this.updateProfile);
-
-    const res = await this.http.post(
-      'https://api.pets-like.com/d1/contents/0',
-      {
+  onproductUpload() {
+    this.authRequest
+      .request$('POST', 'https://api.pets-like.com/d1', '/contents/0', {
         category: this.selectedValue,
-        subject: 'subject string',
+        subject: this.subject,
         images: this.imagestoShow,
         text: this.message,
-        tags: this.strings,
-        extra: {
-          price: 0,
-        },
-      },
-      {
-        headers: {
-          'X-Lemon-Identity':
-            'IQoJb3JpZ2luX2VjEMP//////////wEaDmFwLW5vcnRoZWFzdC0yIkgwRgIhAP6XRk5yo8H2ZgFpLToXUlNOK5RnfCv1owzsgZ77iOQFAiEA22hQU/kkk7cxp4FBJcjYMb04RKE9O6TgOAh/ILboRhgqowQIvP//////////ARADGgwwODU0MDM2MzQ3NDYiDPmIjwFpaM7quYDnMCr3A1b4Htur9mfqGpU6oiCMFJkLbD5mOxCysf2fRWfiBH0Qu/YJJljN9oWDlElaqXhO29RxGP4I+DAh7R6xR5ZFFt76zj4G8wTW/0m6owS9TT66iUVnm2QL0Bxg/V9ausxHEr3Uyj4RkTLHkFMENKqyewx7/6ZBNen8bRdY6YmCwIwWJcuV/5n3QNOnpK4yOSnpS6EgBRsPQOBxGNTB7lGrZqh44gwsV2smDiYcqVz5+umCn1HZT1MhkltLl+KwER9IkjNNWiFGaNVrRH0zYOxieh2yKmU1tOQkyraoWROd4HhVBb/9wfD9pSpVqtpMRll55BZCk9Yu8ZW6IQLJi3lyNqRtpzkPXMDrOv5VK5dvr2ZWtxwAgRMTLJjl4YxeRiBmW1uk2fax59Z1AnQRCCOEaCxRsLyu61IqyC/ulsNhZ87pGmY9Tw/0tau4c41QBGQW31izjIyj6OrSkNY6gdUkbXN+To0BlaNBpg8YD3cPhHPmwKd6737q11fUHsRa5QNDtaRwWkRAvbTv6h5dM4ErJrd7RE9EVJfXmlnwOlFO5NipXbS198/sEeaO5rZDodNc0RA3mtaAOQeJgShGeCUZiNtVoEfSSd9czKfLjC1oeQT0MVTn9IJWKfTvmnuNoskcdTsgs9MUPYn3BjMjsCFfgFl42greTzhHMIS695QGOoQCSXsxPpHcp1DSQt/iS4BNVWeLhiSdHO6kkxQuQ+rTOPUF9hJ7UXBlO6XZWUWB2aLZ90jm78zXs4UrNLnpdXyBvyy9tuPNNyW/3vWNw3GmiFZrgp+WQcV3tGAjOH8TL6iuVWkiq4ywrbKNwC7aXt85mkKu4y+zECENEvdXuzxUtUg19yegy9Oj6hxIzOqT86MrBRuCTLT4dCmiYf6Cz0KU9VXtlkBUnf5g9Zum5NC0zy9drQNg5TKhEzLjme++QqxuTr0kD9Rb8FuPIX63Mekq+7D221LHONywi+gy9KWT+kMsqTj9mJiSihKcDNMdj/5lpnuqFDwmXNhahNwqkxyyHaD9E+o=',
-        },
-      }
-    );
-
-    // const resData = await res.json()
-    console.log(res);
-    res.subscribe((data) => {
-      console.log(data);
-    });
+        tags: this.tags,
+      })
+      .pipe(toArray())
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
-
-  // ngOnInit(): void {}
 }
